9d8492ec5048df7f3707a5d8fa503bd4d3a871db
Anders Blehr <ablehr@gmail.com>
2012-03-03 22:33:36 +0100

    Finally persisting

---
+++ ScolaApp/ScHouseholdResidency.h
@@ -0,0 +1,26 @@
+//
+//  ScHouseholdResidency.h
+//  ScolaApp
+//
+//  Created by Anders Blehr on 03.03.12.
+//  Copyright (c) 2012 Rhelba Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "ScCachedEntity.h"
+
+@class ScHousehold, ScScolaMember;
+
+@interface ScHouseholdResidency : ScCachedEntity
+
+@property (nonatomic, retain) NSNumber * isPartTime;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * switchDay;
+@property (nonatomic, retain) NSNumber * switchFrequency;
+@property (nonatomic, retain) NSNumber * presentOn01Jan;
+@property (nonatomic, retain) NSNumber * daysAtATime;
+@property (nonatomic, retain) ScHousehold *household;
+@property (nonatomic, retain) ScScolaMember *resident;
+
+@end
---
+++ ScolaApp/ScHouseholdResidency.m
@@ -0,0 +1,25 @@
+//
+//  ScHouseholdResidency.m
+//  ScolaApp
+//
+//  Created by Anders Blehr on 03.03.12.
+//  Copyright (c) 2012 Rhelba Software. All rights reserved.
+//
+
+#import "ScHouseholdResidency.h"
+#import "ScHousehold.h"
+#import "ScScolaMember.h"
+
+
+@implementation ScHouseholdResidency
+
+@dynamic isPartTime;
+@dynamic name;
+@dynamic switchDay;
+@dynamic switchFrequency;
+@dynamic presentOn01Jan;
+@dynamic daysAtATime;
+@dynamic household;
+@dynamic resident;
+
+@end
