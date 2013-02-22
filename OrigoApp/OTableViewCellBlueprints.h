6d77e7d15a9827bf6af812a304992654009d6279
Anders Blehr <ablehr@gmail.com>
2013-02-22 09:45:05 +0100

    - Final commit and merge onto master before burrowing into school class   origo nitty gritties.

---
+++ OrigoApp/OTableViewCellBlueprints.h
@@ -0,0 +1,21 @@
+//
+//  OTableViewCellBlueprints.h
+//  OrigoApp
+//
+//  Created by Anders Blehr on 22.02.13.
+//  Copyright (c) 2013 Rhelba Creations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OTableViewCellBlueprints : NSObject
+
++ (NSString *)titleKeyForReuseIdentifier:(NSString *)reuseIdentifier;
++ (NSString *)titleKeyForEntityClass:(Class)entityClass;
++ (NSArray *)detailKeysForReuseIdentifier:(NSString *)reuseIdentifier;
++ (NSArray *)detailKeysForEntityClass:(Class)entityClass;
+
++ (BOOL)titleHasPhotoForEntityClass:(Class)entityClass;
++ (BOOL)requiresTextViewForKey:(NSString *)key;
+
+@end
